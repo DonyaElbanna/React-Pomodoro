222c780c498e30552a5c9138f65add75fa927c4d
Donya Elbanna <donyaelbanna@yahoo.com>
2022-12-13 20:23:26 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="labels-container">
+        <div id="break-label">
+          <h1>Break</h1>
+          <button id="break-decrement">decrease break time</button>
+          <button id="break-increment">increase break time</button>
+          <div id="break-length">break length (5m)</div>
+        </div>
+        <div id="session-label">
+          <h1>Session</h1>
+          <button id="session-decrement">decrease session time</button>
+          <button id="break-increment">increase break time</button>
+          <div id="session-length">session length (25m)</div>
+        </div>
+      </div>
+      <div id="timer">
+        <div id="timer-label">
+        <h1>Session</h1>
+        <div id="time-left">time left (mm:ss)</div>
+      </div>
+      <div id="timer-control">
+        <button id="start_stop">start/stop</button>
+        <button id="reset">reset</button>
+      </div>
+      </div>
+      
     </div>
   );
 }
