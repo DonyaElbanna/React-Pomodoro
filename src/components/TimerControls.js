7166227851eb9501404a009f197cb62372a0f520
Donya Elbanna <donyaelbanna@yahoo.com>
2022-12-16 14:20:32 +0200

    added icons

--- src/components/TimerControls.js
+++ src/components/TimerControls.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "../App.css";
-import play from "../play.png";
-import pause from "../pause.png";
-import reset from "../reset.png";
+// import play from "../play.png";
+// import pause from "../pause.png";
+// import reset from "../reset.png";
 
 function TimerControls({
   message,
@@ -19,13 +19,13 @@ function TimerControls({
         <button id="start-stop" onClick={startStop}>
           {(timerTitle === "Session" && sessionStart) ||
           (timerTitle === "Break" && breakStart) ? (
-            <img src={pause} alt="pause button" style={{ width: "25px" }}></img>
+            <span class="material-symbols-outlined icon">pause_circle</span>
           ) : (
-            <img src={play} alt="play button" style={{ width: "25px" }}></img>
+            <span class="material-symbols-outlined icon">play_circle</span>
           )}
         </button>
         <button id="reset" onClick={resetTimer}>
-          <img src={reset} alt="reset button" style={{ width: "25px" }}></img>
+          <span class="material-symbols-outlined icon">device_reset</span>
         </button>
       </div>
     </div>
