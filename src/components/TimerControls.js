59649f444386eed439a2bf6270311c3c6f4fed7b
Donya Elbanna <donyaelbanna@yahoo.com>
2022-12-24 07:34:04 +0200

    added tooltips

--- src/components/TimerControls.js
+++ src/components/TimerControls.js
@@ -3,6 +3,8 @@ import "../App.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Tooltip from "react-bootstrap/Tooltip";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 function TimerControls({
   message,
@@ -15,6 +17,12 @@ function TimerControls({
   sessionSecondsLeft,
   breakSecondsLeft,
 }) {
+  const startTooltip = <Tooltip id="startTooltip">Start Timer</Tooltip>;
+  const pauseTooltip = <Tooltip id="pauseTooltip">Pause Timer</Tooltip>;
+  const resetTooltip = <Tooltip id="resetTooltip">Reset Timer</Tooltip>;
+  const muteTooltip = <Tooltip id="muteTooltip">Mute</Tooltip>;
+  const unmuteTooltip = <Tooltip id="unmuteTooltip">Unmute</Tooltip>;
+
   return (
     <div id="timer-control">
       <div id="message">{message ? message : <br></br>}</div>
@@ -34,33 +42,43 @@ function TimerControls({
                 }
               >
                 {active ? (
-                  <span className="material-symbols-outlined icon">
-                    pause_circle
-                  </span>
+                  <OverlayTrigger placement="top" overlay={pauseTooltip}>
+                    <span className="material-symbols-outlined icon">
+                      pause_circle
+                    </span>
+                  </OverlayTrigger>
                 ) : (
-                  <span className="material-symbols-outlined icon">
-                    play_circle
-                  </span>
+                  <OverlayTrigger placement="top" overlay={startTooltip}>
+                    <span className="material-symbols-outlined icon">
+                      play_circle
+                    </span>
+                  </OverlayTrigger>
                 )}
               </button>
             </Col>
             <Col>
-              <button id="reset" onClick={reset}>
-                <span className="material-symbols-outlined icon">
-                  device_reset
-                </span>
-              </button>
+              <OverlayTrigger placement="top" overlay={resetTooltip}>
+                <button id="reset" onClick={reset}>
+                  <span className="material-symbols-outlined icon">
+                    device_reset
+                  </span>
+                </button>
+              </OverlayTrigger>
             </Col>
             <Col>
               <button onClick={mute} id="mute">
                 {volume === 0 ? (
-                  <span className="material-symbols-outlined icon">
-                    volume_up
-                  </span>
+                  <OverlayTrigger placement="top" overlay={unmuteTooltip}>
+                    <span className="material-symbols-outlined icon">
+                      volume_up
+                    </span>
+                  </OverlayTrigger>
                 ) : (
-                  <span className="material-symbols-outlined icon">
-                    volume_off
-                  </span>
+                  <OverlayTrigger placement="top" overlay={muteTooltip}>
+                    <span className="material-symbols-outlined icon">
+                      volume_off
+                    </span>
+                  </OverlayTrigger>
                 )}
               </button>
             </Col>
